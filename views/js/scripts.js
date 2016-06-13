2487cfdb7452856188e3cfd9ab846516d6511dae
Jamie Liao <j.liao5079@gmail.com>
2016-06-13 12:32:48 -0400

    jld

--- views/js/scripts.js
+++ views/js/scripts.js
@@ -1,12 +1,30 @@
 $(document).ready(function(){
 
-  if (location.href == "http://iaora.github.io/"){
-    $('#name').addClass('animated bounceIn');
+  if (location.href == "file:///home/jamie/School/Projects/iaora.github.io/index.html"){
+    $('#name').addClass('animated fadeInLeft');
+  }else{
+    $('#name').addClass('animated fadeOutLeft');
+
+    $('navbar').addClass('animated  fadeInRight');
   }
 
-  animationClick('#about', 'tada');
-  animationClick('#projects', 'tada');
-  animationClick('#experience', 'tada');
+
+
+
+  function animationHover(element, animation){
+    element = $(element);
+    element.hover(
+      function() {
+        element.addClass('animated ' + animation);
+      },
+      function(){
+        //wait for animation to finish before removing classes
+        window.setTimeout( function(){
+          element.removeClass('animated ' + animation);
+        }, 2000);
+      }
+    );
+  };
 
   function animationClick(element, animation){
     element = $(element);
